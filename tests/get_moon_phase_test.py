f85fd28edd33b8c10669259d37cb7d6787d5226f
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-05-29 13:12:48 +0200

    Change self.ephemeris for self.eph, to avoid confusing with ephemeris modules's and Ephemeris class's name

--- tests/get_moon_phase_test.py
+++ tests/get_moon_phase_test.py
@@ -5,17 +5,17 @@ from astrobot import ephemeris
 
 class TestEphemeris(unittest.TestCase):
     def setUp(self):
-        self.ephemeris = ephemeris.Ephemeris(48.8566, 2.3522, 0, 'Europe/Paris')
+        self.eph = ephemeris.Ephemeris(48.8566, 2.3522, 0, 'Europe/Paris')
 
     def test_get_moon_phase(self):
         # Test for a full moon
         date1 = datetime.datetime(2024, 6, 22)
-        phase1 = self.ephemeris.get_moon_phase(date1)
+        phase1 = self.eph.get_moon_phase(date1)
         self.assertAlmostEqual(phase1, 180.0, delta=10)
 
         # Test for a new moon
         date2 = datetime.datetime(2024, 6, 6)
-        phase2 = self.ephemeris.get_moon_phase(date2)
+        phase2 = self.eph.get_moon_phase(date2)
         if phase2 < 180:
             self.assertAlmostEqual(phase2, 0.0, delta=10)
         else:
@@ -23,12 +23,12 @@ class TestEphemeris(unittest.TestCase):
 
         # Test for a first quarter moon
         date3 = datetime.datetime(2024, 5, 15)
-        phase3 = self.ephemeris.get_moon_phase(date3)
+        phase3 = self.eph.get_moon_phase(date3)
         self.assertAlmostEqual(phase3, 90, delta=10)
 
         # Test for a last quarter moon
         date4 = datetime.datetime(2024, 5, 30)
-        phase4 = self.ephemeris.get_moon_phase(date4)
+        phase4 = self.eph.get_moon_phase(date4)
         self.assertAlmostEqual(phase4, 270, delta=10)
 
 if __name__ == '__main__':
