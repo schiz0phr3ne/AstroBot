b165895489c354eecd4fd0f23d2e819c3dd32626
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-05-31 14:20:12 +0200

    Test code for pycord slash command

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
 
 import discord
+from discord import Option
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,10 +12,22 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    cogs = ['']
+    cogs = []
     for cog in cogs:
         bot.load_extension(f'astrobot.cogs.{cog}')
         print(f'Loaded cog {cog}')
     print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def sun(
+    ctx,
+    latitude: Option(float),
+    longitude: Option(float),
+    altitude: Option(int, default=0),
+    date: Option(int, default=datetime.now().strftime('%d')),
+    month: Option(int, default=datetime.now().strftime('%m')),
+    year: Option(int, default=datetime.now().strftime('%Y'), )
+):
+    await ctx.respond([latitude, longitude, altitude, date, month, year])
 
 bot.run(DISCORD_TOKEN)
