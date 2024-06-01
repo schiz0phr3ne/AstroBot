2ee0b00c41e1d13f42b9dc77c54d4e913f1bd215
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-01 22:21:38 +0200

    Move sun ephemerides to cogs

---
+++ astrobot/cogs/sun.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from datetime import datetime
+
+import discord
+from discord import Option
+from discord.ext import commands
+
+from ephemeris import Ephemeris
+
+class Sun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.slash_command()
+    async def sun(
+        self,
+        ctx,
+        latitude: Option(float,description='Latitude of the location'),
+        longitude: Option(float, description='Longitude of the location'),
+        altitude: Option(int, default=0, description='Altitude of the location'),
+        day: Option(int, default=int(datetime.now().strftime('%d')), description='Day of the month (default: today)'),
+        month: Option(int, default=int(datetime.now().strftime('%m')), description='Month of the year (default: this month)'),
+        year: Option(int, default=int(datetime.now().strftime('%Y')), description='Year (default: this year)')
+    ):
+        eph = Ephemeris(latitude, longitude, altitude, 'Europe/Paris')
+        sunrise = eph.get_sunrise_time(datetime(year, month, day))
+        sunset = eph.get_sunset_time(datetime(year, month, day))
+        await ctx.respond([latitude, longitude, altitude, day, month, year, sunrise, sunset])
+
+def setup(bot):
+    bot.add_cog(Sun(bot))
--- main.py
+++ main.py
@@ -14,25 +14,14 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    cogs = []
+    """
+    Load cogs and sync commands when the bot is ready.
+    """
+    cogs = ['sun']
     for cog in cogs:
         bot.load_extension(f'astrobot.cogs.{cog}')
-        print(f'Loaded cog {cog}')
+        print(f'Loaded cog: {cog}')
+    await bot.sync_commands()
     print(f'Logged in as {bot.user}')
-
-@bot.command()
-async def sun(
-    ctx,
-    latitude: Option(float, description='Latitude of the location'),
-    longitude: Option(float, description='Longitude of the location'),
-    altitude: Option(int, default=0, description='Altitude of the location'),
-    day: Option(int, default=int(datetime.now().strftime('%d')), description='Day of the month (default: today)'),
-    month: Option(int, default=int(datetime.now().strftime('%m')), description='Month of the year (default: this month)'),
-    year: Option(int, default=int(datetime.now().strftime('%Y')), description='Year (default: this year)')
-):
-    eph = Ephemeris(latitude, longitude, altitude, 'Europe/Paris')
-    sunrise = eph.get_sunrise_time(datetime(year, month, day))
-    sunset = eph.get_sunset_time(datetime(year, month, day))
-    await ctx.respond([latitude, longitude, altitude, day, month, year, sunrise, sunset])
 
 bot.run(DISCORD_TOKEN)
