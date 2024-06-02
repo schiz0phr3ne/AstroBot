af35fe9d6c7b3956fa8977690ca9e13cfe9cba1b
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-02 15:50:31 +0200

    Usable answer from 'sun' cog

--- astrobot/cogs/sun.py
+++ astrobot/cogs/sun.py
@@ -5,8 +5,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from datetime import datetime
 
 import discord
-from discord import Option
+from discord import Embed, Option
 from discord.ext import commands
+from skyfield.api import Angle
 
 from ephemeris import Ephemeris
 
@@ -28,7 +29,25 @@ class Sun(commands.Cog):
         eph = Ephemeris(latitude, longitude, altitude, 'Europe/Paris')
         sunrise = eph.get_sunrise_time(datetime(year, month, day))
         sunset = eph.get_sunset_time(datetime(year, month, day))
-        await ctx.respond([latitude, longitude, altitude, day, month, year, sunrise, sunset])
+
+        latitude_dms = Angle(degrees=latitude).dstr(format=u'{0}{1}°{2:02}′{3:02}.{4:0{5}}″')
+        longitude_dms = Angle(degrees=longitude).dstr(format=u'{0}{1}°{2:02}′{3:02}.{4:0{5}}″')
+        latitude_position = 'N' if latitude >= 0 else 'S'
+        longitude_position = 'E' if longitude >= 0 else 'W'
+        
+        google_maps_url = f'https://www.google.com/maps/place/{latitude_dms}{latitude_position}+{longitude_dms}{longitude_position}'
+        bing_maps_url = f'https://www.bing.com/maps?cp={latitude}~{longitude}&lvl=17'
+
+        embed = Embed(
+            title='Éphémérides du soleil',
+            description=f'Pour la date du {day}/{month}/{year} à {latitude}° de latitude et {longitude}° de longitude.',
+            color=discord.Color.gold()
+        )
+        embed.add_field(name='Lever du soleil', value=sunrise.strftime('%H:%M:%S'), inline=False)
+        embed.add_field(name='Coucher du soleil', value=sunset.strftime('%H:%M:%S'), inline=False)
+        embed.add_field(name='Cartes', value=f'[Google Maps]({google_maps_url}) - [Bing Maps]({bing_maps_url})', inline=False)
+
+        await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(Sun(bot))
