ab1ed2d048800848fd42a8ec8802d0a265a3694c
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-04 17:10:51 +0200

    Add AstrobinIotd in Pictures cog

--- astrobot/cogs/pictures.py
+++ astrobot/cogs/pictures.py
@@ -10,11 +10,82 @@ from discord import Embed
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
-from constants import NASA_API_APOD_URL, NASA_APOD_URL, NASA_LOGO_URL
+from constants import (
+    ASTROBIN_API_IOTD_URL,
+    ASTROBIN_BASE_URL,
+    ASTROBIN_LOGO_URL,
+    ASTROBIN_API_URL,
+    ASTROBIN_USERS_URL,
+    NASA_API_APOD_URL,
+    NASA_APOD_URL,
+    NASA_LOGO_URL,
+)
 
 load_dotenv()
-NASA_API_KEY = os.getenv("NASA_API_KEY")
+ASTROBIN_API_KEY = os.getenv("ASTROBIN_API_KEY")
+ASTROBIN_API_SECRET = os.getenv("ASTROBIN_API_SECRET")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+NASA_API_KEY = os.getenv("NASA_API_KEY")
+
+class AstrobinIotd(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.send_astrobin_iotd.start()
+
+    @tasks.loop(time=time(hour=9, minute=0, second=0, tzinfo=ZoneInfo('Europe/Paris')))
+    async def send_astrobin_iotd(self):
+        channel = self.bot.get_channel(CHANNEL_ID)
+        astrobin_api_url = f'{ASTROBIN_BASE_URL}/{ASTROBIN_API_URL}{ASTROBIN_API_IOTD_URL}'
+        payload = {
+            'api_key': ASTROBIN_API_KEY,
+            'api_secret': ASTROBIN_API_SECRET,
+            'format': 'json',
+        }
+
+        try:
+            response = requests.get(astrobin_api_url, params=payload, timeout=10)
+            response.raise_for_status()
+            data = response.json()
+            astrobin_iotd_url = f'{ASTROBIN_BASE_URL}/{data["objects"][0]["image"]}'
+
+            try:
+                response = requests.get(astrobin_iotd_url, params=payload, timeout=10)
+                response.raise_for_status()
+                data = response.json()
+            
+                title = data['title']
+                description = data['description']
+                image_url = f'{ASTROBIN_BASE_URL}/{data["hash"]}'
+                embed_image_url = data['url_hd']
+                author = data['user']
+                author_url = f'{ASTROBIN_USERS_URL}{author}'
+
+                if len(description) > 1024:
+                    description = description[:1021] + '...'
+
+                embed = Embed(
+                    title=f'Astrobin IOTD du {datetime.now().strftime("%d/%m/%Y")}',
+                    url=image_url
+                )
+                embed.add_field(name=title, value=description, inline=False)
+                embed.add_field(name=f'Par {author}', value=author_url, inline=False)
+                embed.set_thumbnail(url=ASTROBIN_LOGO_URL)
+                embed.set_image(url=embed_image_url)
+
+                await channel.send(embed=embed)
+            except requests.exceptions.RequestException as e:
+                await channel.send(f'Erreur lors de la récupération de l\'IOTD d\'Astrobin (étape 2) : {e}')
+        except requests.exceptions.RequestException as e:
+            await channel.send(f'Erreur lors de la récupération de l\'IOTD d\'Astrobin (étape 1) : {e}')
+
+    @send_astrobin_iotd.before_loop
+    async def before_send_astrobin_iotd(self):
+        await self.bot.wait_until_ready()
+
+    @send_astrobin_iotd.error
+    async def on_send_astrobin_iotd_error(self, error):
+        print(f'An error occurred in the send_astrobin_iotd task: {error}')
 
 class NasaApod(commands.Cog):
     
@@ -26,43 +97,44 @@ class NasaApod(commands.Cog):
     async def send_nasa_apod(self):
         payload = {'api_key': NASA_API_KEY}
         channel = self.bot.get_channel(CHANNEL_ID)
-        
+
         try:
             response = requests.get(NASA_API_APOD_URL, params=payload, timeout=10)
             response.raise_for_status()
             data = response.json()
-            
+
             title = data['title']
             explanation = data['explanation']
             media_type = data['media_type']
             media_url = data['url']
-            
+
             if len(explanation) > 1024:
                 explanation = explanation[:1021] + '...'
-            
+
             embed = Embed(
                 title=f'NASA APOD du {datetime.now().strftime("%d/%m/%Y")}',
                 url=NASA_APOD_URL
             )
             embed.add_field(name=title, value=explanation, inline=False)
             embed.set_thumbnail(url=NASA_LOGO_URL)
-            
+
             if media_type == 'image':
                 embed.set_image(url=media_url)
             else:
                 embed.add_field(name='Lien vers la vidéo :', value=media_url, inline=False)
-            
+
             await channel.send(embed=embed)
         except requests.exceptions.RequestException as e:
-            await channel.send(f'Erreur lors de la récupération de l\'APOD de la NASA: {e}')
-    
+            await channel.send(f'Erreur lors de la récupération de l\'APOD de la NASA : {e}')
+
     @send_nasa_apod.before_loop
     async def before_send_nasa_apod(self):
         await self.bot.wait_until_ready()
-    
+
     send_nasa_apod.error
     async def on_send_nasa_apod_error(self, error):
         print(f'An error occurred in the send_nasa_apod task: {error}')
 
 def setup(bot):
+    bot.add_cog(AstrobinIotd(bot))
     bot.add_cog(NasaApod(bot))
--- astrobot/constants.py
+++ astrobot/constants.py
@@ -1,3 +1,8 @@
+ASTROBIN_API_IOTD_URL = 'imageoftheday/?limit=1'
+ASTROBIN_API_URL = 'api/v1/'
+ASTROBIN_BASE_URL = 'https://astrobin.com'
+ASTROBIN_LOGO_URL = 'https://cdn.astrobin.com/static/astrobin/images/astrobin-logo-small.39b63752c38d.png'
+ASTROBIN_USERS_URL = 'https://astrobin.com/users/'
 NASA_API_APOD_URL = 'https://api.nasa.gov/planetary/apod'
 NASA_APOD_URL = 'https://apod.nasa.gov/apod/astropix.html'
 NASA_LOGO_URL = 'https://gpm.nasa.gov/sites/default/files/document_files/NASA-Logo-Large.png'
