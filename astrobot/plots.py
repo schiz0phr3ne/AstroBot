423c9d4979c4465c0b8f1f1ed6db17499f59199d
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-09 22:16:14 +0200

    chore: Optimize polar sky plot and improve code organization

--- astrobot/plots.py
+++ astrobot/plots.py
@@ -1,17 +1,29 @@
-from matplotlib import pyplot as plt
+from io import BytesIO
 
-from .constants import DIRECTIONS
+import numpy as np
+from matplotlib.pyplot import plt
+
+from constants import DIRECTIONS
 
 def plot_polar_sky(
-    
+
 ):
     """
     Plot a polar sky map of the celestial sphere.
     """
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.set_theta_zero_location('N')
+    ax.set_theta_zero_location("S")
     ax.set_theta_direction(-1)
-    ax.set_xticks([i for i in range(0, 360, 45)], DIRECTIONS)
-    ax.set_ylim(0, 90)
-    ax.set_yticks([i for i in range(0, 100, 10)])
-    
+    ax.set_xticks(np.pi/180 * np.linspace(0, 360, 8, endpoint=False), DIRECTIONS)
+    ax.set_rmax(90)
+    ax.set_rticks(list(range(90, -10, -10)), [f"{i}°" for i in range(90, -10, -10)])
+    ax.tick_params(axis='y', labelsize=8)
+    ax.grid(True)
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image = buffer.getvalue()
+    buffer.close()
+
+    return image
