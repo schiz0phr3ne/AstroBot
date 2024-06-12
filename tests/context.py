5379876f6027182a688071ce4a99c067f286f488
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-12 21:35:52 +0200

    chore: Add docstring to context.py

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,13 @@
+"""
+This module provides the necessary context for running tests for the AstroBot project.
+
+It adds the project directory to the sys.path, allowing the tests to import the required modules.
+
+Usage:
+    import context
+
+"""
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
