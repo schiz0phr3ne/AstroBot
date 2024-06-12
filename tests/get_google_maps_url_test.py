12634e8b02c2ffbfecb58e0961fe499ca00a8913
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-12 21:45:45 +0200

    chore: Refactor unit tests for get_google_maps_url function in utils module

--- tests/get_google_maps_url_test.py
+++ tests/get_google_maps_url_test.py
@@ -1,9 +1,34 @@
+"""
+This file contains the tests for the get_google_maps_url function in the utils module.
+The utils module provides utility functions for the AstroBot project.
+
+Attributes:
+    None
+
+Methods:
+    test_get_google_maps_url: Test the get_google_maps_url function.
+"""
+
 import unittest
 from context import astrobot
 from astrobot import utils
 
 class TestGetGoogleMapsUrl(unittest.TestCase):
+    """
+    Test the get_google_maps_url function in the utils module.
+    
+    Attributes:
+        None
+        
+    Methods:
+        test_get_google_maps_url: Test the get_google_maps_url function.
+    """
     def test_get_google_maps_url(self):
+        """
+        Test case for the get_google_maps_url function.
+        It verifies that the function correctly generates a Google Maps URL
+        for a given latitude and longitude.
+        """
         url = utils.get_google_maps_url(48.8566, 2.3522)
         self.assertEqual(url, 'https://www.google.com/maps/place/48°51′23.8″N+2°21′07.9″E')
 
