f4026d4128d01a4329510a940d0af2ea4b91e753
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-12 21:40:09 +0200

    chore: Refactor unit tests for get_bing_maps_url function

--- tests/get_bing_maps_url_test.py
+++ tests/get_bing_maps_url_test.py
@@ -1,9 +1,34 @@
+"""
+This script tests the get_bing_maps_url function in the utils module.
+The utils module provides utility functions for the AstroBot project.
+
+Attributes:
+    None
+
+Methods:
+    test_get_bing_maps_url: Test the get_bing_maps_url function.
+"""
+
 import unittest
 from context import astrobot
 from astrobot import utils
 
 class TestGetBingMapsUrl(unittest.TestCase):
+    """
+    Test the get_bing_maps_url function in the utils module.
+    
+    Attributes:
+        None
+    
+    Methods:
+        test_get_bing_maps_url: Test the get_bing_maps_url function.
+    """
     def test_get_bing_maps_url(self):
+        """
+        Test case for the get_bing_maps_url function.
+        It verifies that the function correctly generates a Bing Maps URL
+        for a given latitude and longitude.
+        """
         url = utils.get_bing_maps_url(48.8566, 2.3522)
         self.assertEqual(url, 'https://www.bing.com/maps?cp=48.8566~2.3522&lvl=17')
 
