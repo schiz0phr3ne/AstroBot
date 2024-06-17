7c0ccfdc41acd5cc18bdc91d6b99bcc630291eba
Lucas Mourey <22858496+schiz0phr3ne@users.noreply.github.com>
2024-06-17 17:31:13 +0200

    chore: (WIP) Add option to choose plot type in Planets cog

--- astrobot/cogs/planets.py
+++ astrobot/cogs/planets.py
@@ -17,12 +17,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from datetime import datetime
 
 import discord
-from discord import Embed, Option
+from discord import Embed, File, Option
 from discord.ext import commands
 
-import utils
-from constants import PLANETS
 from ephemeris import Ephemeris
+from constants import PLANETS, PLOT_TYPES
+import plots
+import utils
 
 class Planets(commands.Cog):
     """
@@ -50,6 +51,7 @@ class Planets(commands.Cog):
         latitude: Option(float, description='Latitude of the location'),
         longitude: Option(float, description='Longitude of the location'),
         altitude: Option(int, default=0, description='Altitude of the location'),
+        plot_type: Option(str, choices=PLOT_TYPES.keys(), default='Polaire', description='Type of plot (default: polar sky)'),
         day: Option(int, default=0, min_value=1, max_value=31, description='Day of the month (default: today)'),
         month: Option(int, default=0, min_value=1, max_value=12, description='Month of the year (default: this month)'),
         year: Option(int, default=0, min_value=1550, max_value=2650, description='Year (default: this year)')
@@ -79,6 +81,11 @@ class Planets(commands.Cog):
 
         eph = Ephemeris(latitude, longitude, altitude, 'Europe/Paris')
 
+        if day != 0 and month != 0 and year != 0:
+            custom_date = True
+        else:
+            custom_date = False
+
         current_datetime = datetime.now()
         day = current_datetime.day if day == 0 else day
         month = current_datetime.month if month == 0 else month
@@ -90,16 +97,29 @@ class Planets(commands.Cog):
         google_maps_url = utils.get_google_maps_url(latitude, longitude)
         bing_maps_url = utils.get_bing_maps_url(latitude, longitude)
 
+        # If the date is not the current date, compute the sky map for the custom date (at midnight)
+        if not custom_date:
+            compute_datetime = current_datetime
+        else:
+            compute_datetime = datetime(year, month, day)
+
+        # Plot the polar sky map or the xy path of the planet
+        if plot_type == 'Polaire':
+            file = File(plots.plot_polar_sky(eph, planet, compute_datetime), filename='polar_sky.png')
+        else:
+            file = File(plots.plot_xy_path(eph, planet, compute_datetime), filename='xy_path.png')
+
         embed = Embed(
             title=f'Éphémérides de la planète {planet}',
             description=f'Pour la date du {day}/{month}/{year} à {latitude}° de latitude et {longitude}° de longitude.',
             color=discord.Color.teal()
         )
+        embed.set_image(url=f'attachment://{file.filename}')
         embed.add_field(name='Lever de la planète', value=planetrise.strftime('%H:%M:%S'), inline=False)
         embed.add_field(name='Coucher de la planète', value=planetset.strftime('%H:%M:%S'), inline=False)
         embed.add_field(name='Cartes du lieu d\'observation', value=f'[Google Maps]({google_maps_url}) - [Bing Maps]({bing_maps_url})', inline=False)
 
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=embed, file=file)
 
 def setup(
     bot
